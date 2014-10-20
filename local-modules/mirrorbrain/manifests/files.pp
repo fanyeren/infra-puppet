b44cc26285ac094da191f8f549f88e3ccb8b2e9d
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-10-20 10:43:29 -0700

    These files are not manageable by Puppet today

--- local-modules/mirrorbrain/manifests/files.pp
+++ local-modules/mirrorbrain/manifests/files.pp
@@ -8,21 +8,22 @@ class mirrorbrain::files {
                        ],
             source  => 'puppet:///modules/mirrorbrain/geoip.conf';
 
-        '/etc/apache2/mods-available/dbd.conf' :
-            ensure  => present,
-            require => Class['apache2'],
-            source  => [
-                        'puppet:///modules/mirrorbrain/dbd.conf.private',
-                        'puppet:///modules/mirrorbrain/dbd.conf'];
-
-
+# This file contains password and cannot be currently managed
+#        '/etc/apache2/mods-available/dbd.conf' :
+#            ensure  => present,
+#            require => Class['apache2'],
+#            source  => [
+#                        'puppet:///modules/mirrorbrain/dbd.conf.private',
+#                        'puppet:///modules/mirrorbrain/dbd.conf'];
+#
+#        '/etc/mirrorbrain.conf' :
+#            ensure  => present,
+#            source  => 'puppet:///modules/mirrorbrain/mirrorbrain.conf';
+#
         '/etc/mirmon.conf' :
             ensure  => present,
             source  => 'puppet:///modules/mirrorbrain/mirmon.conf';
 
-        '/etc/mirrorbrain.conf' :
-            ensure  => present,
-            source  => 'puppet:///modules/mirrorbrain/mirrorbrain.conf';
     }
 
     apache2::virtualhost {
