85326945c53f55cbc16947a4b06c06e8eda46cb0
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-03-24 22:20:10 -0700

    place ssl.conf into Apache config directory as well

--- local-modules/ssl-cert/manifests/init.pp
+++ local-modules/ssl-cert/manifests/init.pp
@@ -4,7 +4,7 @@
 class ssl-cert {
   include apache2
 
-  # certificates
+  # certificates and apache config to let Apache recognize this file
   file {
     '/etc/apache2/certificate.crt':
       source => "puppet:///modules/${module_name}/${hostname}.crt",
@@ -12,6 +12,10 @@ class ssl-cert {
       ;
     '/etc/apache2/gd_bundle.crt':
       source => "puppet:///modules/${module_name}/gd_bundle.crt",
+      notify  => Exec['reload-apache2']
+      ;
+    '/etc/apache2/conf.d/ssl.conf':
+      source => "puppet:///modules/${module_name}/ssl.conf",
       notify  => Exec['reload-apache2']
       ;
   }
