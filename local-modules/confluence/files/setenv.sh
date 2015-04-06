e976b71eed34abff868931c0a25491cefd2bc976
Kohsuke Kawaguchi <kk@kohsuke.org>
2015-04-05 22:43:45 -0700

    Confluence post switch-over to lettuce.
    Ensure that eggplant will not run confluence, but leave all the other
    files intact on disk in case something bad happens and we need to switch back.

--- local-modules/confluence/files/setenv.sh
+++
@@ -1,6 +0,0 @@
-# sourced by catalina.sh upon start
-
-# read the one shipped by Atlassian first
-. /srv/wiki/current/bin/setenv.sh
-
-export JAVA_OPTS="-Xms512m -Xmx640m -XX:MaxPermSize=192m $JAVA_OPTS -Djava.awt.headless=true "
